a11690ed9a5de7487cd116d3248093dd168ab655
Mike Roitman <mikeroit@gmail.com>
2017-01-09 14:44:44 -0700

    stubbed out function headers so i can start coding

---
+++ ASG1/ASG1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+//Submission Code:--------------------------------------------------------------
+
+//split the array (inp) into words, returning the number of words
+//a sequence of non-blank characters ending in one or more blank spaces
+//is a word
+//argc should be set to the number of words
+//argv should contain the words
+int parse_command(char* inp, int* argc, char* argv[])
+{
+}
+
+//convert value into a number given by specified base
+//store conversion in str
+//if base is 10, and value is negative, include a negative sign in str
+//all other bases should be treated as unsigned
+//return a pointer to the value of str
+char* itoa(int value, char *str, int base)
+{
+}
+
+//write a function called printany that can print any primitive type
+//may use up to two args, no global variables
+void printany()
+{
+}
+
+//write a function that takes 3 ints (base, flag, limit) and a DESC pointer to g
+//populate memory pointed to by g as per stuct in assignment
+void populate_desc(int base, int limit, int flag, DESC *g)
+{
+}
+
+//END Submission Code:-----------------------------------------------------------
+
+int main()
+{
+    printf("Hello world\n");
+    return 0;
+}
+
